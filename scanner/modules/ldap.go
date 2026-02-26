bedf7efcf37965a6fc07543b489733db8bec7793
erra9x <vflame6x@gmail.com>
2026-02-26 22:03:22 +0500

    fix(ldap): wire ProxyAwareDialer for SOCKS5 proxy support
    LDAP module was using net.Dialer directly, bypassing proxy config.
    Now uses dialer.DialAutoContext + ldap.NewConn to route through proxy.
    
    Fixes #125

--- scanner/modules/ldap.go
+++ scanner/modules/ldap.go
@@ -2,7 +2,6 @@ package modules
 
 import (
 	"context"
-	"net"
 	"time"
 
 	"github.com/go-ldap/ldap/v3"
@@ -11,31 +10,23 @@ import (
 
 // LDAPHandler is an implementation of ModuleHandler for LDAP/LDAPS simple bind authentication.
 // Supports plain LDAP (port 389) and LDAPS (port 636) with TLS.
-func LDAPHandler(ctx context.Context, _ *utils.ProxyAwareDialer, timeout time.Duration, target *Target, credential *Credential) (bool, error) {
+// Routes connections through ProxyAwareDialer for SOCKS5 proxy support.
+func LDAPHandler(ctx context.Context, dialer *utils.ProxyAwareDialer, timeout time.Duration, target *Target, credential *Credential) (bool, error) {
 	addr := target.Addr()
 
-	d := &net.Dialer{Timeout: timeout}
-
-	var (
-		conn *ldap.Conn
-		err  error
-	)
-	if target.Encryption {
-		conn, err = ldap.DialURL("ldaps://"+addr, ldap.DialWithDialer(d), ldap.DialWithTLSConfig(utils.GetTLSConfig()))
-	} else {
-		conn, err = ldap.DialURL("ldap://"+addr, ldap.DialWithDialer(d))
-	}
+	netConn, err := dialer.DialAutoContext(ctx, "tcp", addr, target.Encryption)
 	if err != nil {
 		return false, err
 	}
+
+	conn := ldap.NewConn(netConn, target.Encryption)
+	conn.Start()
 	defer func() { _ = conn.Close() }()
 
 	// Close connection if context is cancelled.
 	go func() {
-		select {
-		case <-ctx.Done():
-			_ = conn.Close()
-		}
+		<-ctx.Done()
+		_ = conn.Close()
 	}()
 
 	conn.SetTimeout(timeout)
